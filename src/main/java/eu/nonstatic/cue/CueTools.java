12660894ec7a001f73bb85d53b9238e1cb4fae1f
fabien <fabmars@gmail.com>
2022-05-10 03:36:05 +0200

    Improved CueTools in a way that makes it more flexible, and that allows to open only one stream from a file in Cuereader.

--- src/main/java/eu/nonstatic/cue/CueTools.java
+++ src/main/java/eu/nonstatic/cue/CueTools.java
@@ -5,8 +5,11 @@ import com.ibm.icu.text.CharsetMatch;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -14,24 +17,61 @@ import java.nio.file.Path;
 public class CueTools {
 
   public static final String CUE_EXTENSION = "cue";
-  public static final Charset DEFAULT_CHARSET = Charset.forName("ISO-8859-1");
 
-  public static Charset detectEncoding(Path file) {
+  public static final Charset DEFAULT_CHARSET = StandardCharsets.ISO_8859_1;
+  public static final int DEFAULT_CONFIDENCE = 30;
+
+  private final int confidence;
+  private final Charset fallbackCharset;
+
+  public CueTools() {
+    this(DEFAULT_CONFIDENCE, DEFAULT_CHARSET);
+  }
+
+  public CueTools(int confidence, Charset fallbackCharset) {
+    this.confidence = confidence;
+    this.fallbackCharset = fallbackCharset;
+  }
+
+  public CueContext detectEncoding(File file) {
+    return detectEncoding(file.toPath());
+  }
+
+  public CueContext detectEncoding(Path file) {
+    String name = file.toString();
     try (BufferedInputStream is = new BufferedInputStream(Files.newInputStream(file))) {
-      CharsetDetector cd = new CharsetDetector();
-      cd.setText(is);
-      CharsetMatch cm = cd.detect();
-      if (cm != null && cm.getConfidence() > 30) {
-        return Charset.forName(cm.getName());
-      } else {
-        log.debug("Confidence low. Fallback to {} for {}", DEFAULT_CHARSET, file.toAbsolutePath());
-        return DEFAULT_CHARSET;
-      }
+      return detectEncoding(name, is);
     } catch (IOException e) {
-      log.debug("Fallback to {} for {}: {}", DEFAULT_CHARSET, file.toAbsolutePath(), e.getMessage(), e);
-      return DEFAULT_CHARSET;
+      log.debug("Fallback to {} for {}: {}", fallbackCharset, file.toAbsolutePath(), e.getMessage(), e);
+      return new CueContext(name, fallbackCharset);
     }
   }
+
+  public CueContext detectEncoding(String name, InputStream is) throws IOException {
+    try (BufferedInputStream bis = new BufferedInputStream(is)) {
+      return detectEncoding(name, bis);
+    }
+  }
+
+  /**
+   * icu calls reset() on the stream, so it needs to support mark()
+   */
+  CueContext detectEncoding(String name, BufferedInputStream bis) throws IOException {
+    Charset charset;
+
+    CharsetDetector cd = new CharsetDetector();
+    cd.setText(bis);
+    CharsetMatch cm = cd.detect(); // calls reset() !
+    if (cm != null && cm.getConfidence() > confidence) {
+      charset = Charset.forName(cm.getName());
+    } else {
+      log.debug("Confidence low. Fallback to {} for {}", fallbackCharset, name);
+      charset = fallbackCharset;
+    }
+
+    return new CueContext(name, charset);
+  }
+
 
   public static String unquote(String s) {
     if (s != null) {
@@ -50,11 +90,9 @@ public class CueTools {
     String ext = null;
 
     String fileName = file.getFileName().toString();
-    if (fileName != null) {
-      int dotpos = fileName.lastIndexOf('.');
-      if (dotpos >= 0) {
-        ext = fileName.substring(dotpos + 1);
-      }
+    int dot = fileName.lastIndexOf('.');
+    if (dot >= 0) {
+      ext = fileName.substring(dot + 1);
     }
     return ext;
   }
