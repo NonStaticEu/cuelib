7d7bbbe6ff0ca72bf630dcda7e66e4b3638a5fe4
fabmars <fabien@nonstatic.eu>
2022-12-31 04:05:12 +0100

    Extracted a TimeCode class from the CueIndex logic to make it reusable.

--- src/main/java/eu/nonstatic/cue/CueIndex.java
+++ src/main/java/eu/nonstatic/cue/CueIndex.java
@@ -1,84 +1,79 @@
 package eu.nonstatic.cue;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
 public class CueIndex extends CueEntity {
 
   public static final String KEYWORD = CueWords.INDEX;
 
-  public static final int SECONDS_PER_MINUTE = 60;
-  public static final int MILLIS_PER_SECOND = 1000;
-  public static final int FRAMES_PER_SECOND = 75;
+  @Setter
+  private int number;
+  private TimeCode timeCode;
 
-
-  private final int number;
-
-  private int minutes;
-  private int seconds;
-  private int frames;
 
   public CueIndex(int number, int minutes, int seconds, int frames) {
     this.number = number;
-    setTime(minutes, seconds, frames);
+    this.timeCode = new TimeCode(minutes, seconds, frames);
   }
 
   public CueIndex(int number, String timeCode) {
     this.number = number;
-    setTimeCode(timeCode);
+    this.timeCode = TimeCode.parse(timeCode);
   }
 
+  public int getMinutes() {
+    return timeCode.getMinutes();
+  }
 
-  public void setTime(int minutes, int seconds, int frames) {
-    setMinutes(minutes);
-    setSeconds(seconds);
-    setFrames(frames);
+  public int getSeconds() {
+    return timeCode.getSeconds();
+  }
+
+  public int getFrames() {
+    return timeCode.getFrames();
   }
 
   public void setMinutes(int minutes) {
     if (minutes < 0 || minutes > 99) {
       throw new IllegalArgumentException("minutes must be in the [0-99] range");
     }
-    this.minutes = minutes;
+    timeCode.setMinutes(minutes);
   }
 
   public void setSeconds(int seconds) {
-    if (seconds < 0 || seconds > 59) {
-      throw new IllegalArgumentException("seconds must be in the [0-59] range");
-    }
-    this.seconds = seconds;
+    timeCode.setSeconds(seconds);
   }
 
   public void setFrames(int frames) {
-    if (frames < 0 || frames > 75) {
-      throw new IllegalArgumentException("frames must be in the [0-74] range");
-    }
-    this.frames = frames;
+    timeCode.setFrames(frames);
   }
 
+  public void setTime(int minutes, int seconds, int frames) {
+    timeCode.setTime(minutes, seconds, frames);
+  }
+
+
   public long getTimeMillis() {
-    return (minutes * SECONDS_PER_MINUTE * MILLIS_PER_SECOND)
-        + (seconds * MILLIS_PER_SECOND)
-        + ((frames * MILLIS_PER_SECOND + FRAMES_PER_SECOND - 1) / FRAMES_PER_SECOND); // upper rounding
+    return timeCode.getTimeMillis();
   }
 
   public void setTimeMillis(long millis) {
-    setTime((int) (millis / (SECONDS_PER_MINUTE * MILLIS_PER_SECOND)),
-        (int) ((millis / MILLIS_PER_SECOND) % SECONDS_PER_MINUTE),
-        (int) ((FRAMES_PER_SECOND * (millis % MILLIS_PER_SECOND)) / MILLIS_PER_SECOND));
+    timeCode.setTimeMillis(millis);
   }
 
+
   public String getTimeCode() {
-    return String.format("%02d:%02d:%02d", minutes, seconds, frames);
+    return timeCode.toString();
   }
 
   public void setTimeCode(String timeCode) {
-    String[] parts = timeCode.split(":");
-    setTime(Integer.valueOf(parts[0]), Integer.valueOf(parts[1]), Integer.valueOf(parts[2]));
+    this.timeCode = TimeCode.parse(timeCode);
   }
 
   @Override
   public String toSheetLine() {
-    return String.format("%s %02d %s", KEYWORD, number, getTimeCode());
+    return String.format("%s %02d %s", KEYWORD, number, timeCode.toString());
   }
 }
